d79bbacf1324e4f2dcd60e3ce17ee65e45d535f4
Giorgos Kappes <geokapp@gmail.com>
2014-08-08 12:02:30 +0300

    Changed a file reference in header

--- src/confslice.cc
+++ src/confslice.cc
@@ -8,7 +8,7 @@
  * This is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License version 2.1, as published by the Free Software 
- * Foundation.  See file COPYING.
+ * Foundation.  See file LICENSE.
  * 
  */
 #include <iostream>

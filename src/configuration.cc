f9feb72bb7a9a5b47c5839c2d7ff30928f3f27b9
Giorgos Kappes <geokapp@gmail.com>
2014-08-09 23:28:48 +0300

    fixed get_next_entity()

--- src/configuration.cc
+++ src/configuration.cc
@@ -659,7 +659,7 @@ Entity *Entity::get_next_entity() {
   if (m_it_entities != m_entities.end() && !m_entities.empty()) {
     Entity *entity = *m_it_entities;
     ++m_it_entities;
-    m_keys.pop_front();
+    m_entities.pop_front();
     return entity;
   } else {
     return NULL;
@@ -803,7 +803,7 @@ Entity *Configuration::get_next_entity() {
   if (m_it_entities != m_entities.end() && !m_entities.empty()) {
     Entity *entity = *m_it_entities;
     ++m_it_entities;
-    m_keys.pop_front();
+    m_entities.pop_front();
     return entity;
   } else {
     return NULL;
